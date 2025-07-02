ea9922abff78a0d1df1141ff062d2fffb622ec05
hgfghhccchgtjcdfhhhh <hgfghhccchgtjcdfhhhh@gmail.com>
2025-07-02 11:03:24 +0200

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,20 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
+import AllSpecies from "@/pages/all-species";
+import AllResearch from "@/pages/all-research";
+import ApiDocs from "@/pages/api-docs";
+import EducatorTools from "@/pages/educator-tools";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/species" component={AllSpecies} />
+      <Route path="/research" component={AllResearch} />
+      <Route path="/api-docs" component={ApiDocs} />
+      <Route path="/educator-tools" component={EducatorTools} />
       <Route component={NotFound} />
     </Switch>
   );
